39cf754e2675dbd372151649f577a1478ce91df9
Adilson19 <adilsonsousaas82@gmail.com>
2025-03-31 13:47:57 +0100

    feat: configurando e enviando o primeiro email

---
+++ app/Http/Controllers/Mails/AuthMailController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Mails;
+
+use App\Http\Controllers\Controller;
+use App\Mail\RegisterEmail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class AuthMailController extends Controller
+{
+    //  Metodo para enviar um email
+    public function sendRegisterMail(){
+        //  Criando uma instancia da class mail
+        $registerEmail = new RegisterEmail();
+
+        //  Usando a class Mail do Laravel para fazer o envio dessa instancio de envio de email
+        Mail::to('adilsonsousaas82@gmail.com')->send($registerEmail);
+
+    }
+}
